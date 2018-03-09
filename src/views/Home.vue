59365ace4b69cb0ca769cd983776ad55fdc59b35
Romulo Gabriel Rodrigues <yemolai@gmail.com>
2018-03-09 12:28:11 -0300

    Created preview of panel with user's metas as responsavel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,95 @@
 <template lang="pug">
-  .hello
-    h1 {{ msg }}
-    h2 Rotas
-    ul
-      li
-        a(href="#/painel") Painel
-    .container
-      b-row
-        b-col.text-right
-          b-btn.floating(
-            variant='primary'
-            v-if="can('setor_create')"
-            @click="goTo({name:'AddSetor'})"
-          ) Criar nova diretoria
-      diretorias-table
+  .hello.container.text-left
+    h2 Metas
+    b-table(
+      v-if='!loading && usuario.responsabilidade'
+      :fields='fields'
+      :items='usuario.responsabilidade || []'
+      @row-clicked='goToMeta'
+      stacked='md'
+      hover
+    )
 </template>
 
 <script>
-import DiretoriasTable from '@/components/DiretoriasTable'
+import gql from 'graphql-tag'
+import Formatters from '@/components/Formatters'
+import Helpers from '@/components/Helpers'
 import router from '@/router'
+const fields = [
+  {key: 'titulo', label: 'Título'},
+  {key: 'atualizado', label: 'Atualizado em', formatter: Helpers.timestamp()},
+  {key: 'escopo', formatter: Formatters.escopo},
+  {key: 'prazo', formatter: Formatters.deadline},
+  {key: 'custo', formatter: (v, k, i) => Formatters.cost(i)}
+]
 export default {
   name: 'Home',
-  methods: {
-    can: function (permissao) {
-      return !!this.user.permissoes[permissao]
-    },
-    goTo: (...args) => (router.push(...args))
-  },
   data () {
     return {
-      msg: 'Sistema em desenvolvimento',
       loading: 0,
-      user: { permissoes: { setor_create: true } }
+      usuario: {},
+      fields
     }
   },
-  components: {
-    DiretoriasTable
+  methods: {
+    goToMeta: item => router.push({
+      name: 'Meta',
+      params: {
+        setor: item.coordenadoria.setor.sigla,
+        coordenadoria: item.coordenadoria.sigla,
+        year: new Date(item.createdAt).getFullYear(),
+        meta: item.id
+      }
+    })
+  },
+  apollo: {
+    usuario: {
+      fetchPolicy: 'network-only',
+      query: gql`
+        query getUser($usrId: ID!) {
+          usuario(id: $usrId) {
+            nome
+            usuario
+            permissoes {
+              meta_create
+              meta_update
+              meta_delete
+              own_meta_update
+              own_meta_delete
+            }
+            responsabilidade {
+              id
+              titulo
+              escopo_previsto
+              escopo_realizado
+              inicio_previsto
+              fim_previsto
+              inicio_realizado
+              fim_realizado
+              custo_previsto
+              custo_realizado
+              atualizado
+              createdAt
+              updatedAt
+              coordenadoria {
+                id
+                sigla
+                setor {
+                  id
+                  sigla
+                }
+              }
+            }
+          }
+        }
+      `,
+      variables: function () {
+        return {
+          usrId: 1
+        }
+      }
+    }
   }
 }
 </script>
