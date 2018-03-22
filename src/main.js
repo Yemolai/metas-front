780fabcabd40b20017d56c11dbcd9bb867e9a776
Romulo Gabriel Rodrigues <yemolai@gmail.com>
2018-03-22 14:36:13 -0300

    binding config to external config file

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import VueApollo from 'vue-apollo'
+import cfg from '@/cfg.json'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -14,15 +15,14 @@ import App from './App'
 import router from './router'
 
 const httpLink = new HttpLink({
-  // You should use an absolute URL here
-  uri: 'http://localhost:7700/graphql'
+  uri: cfg.graphqlEndpoint
 })
 
 // Create the apollo client
 const apolloClient = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache(),
-  connectToDevTools: true
+  connectToDevTools: cfg.connectGraphqlToDevTools
 })
 
 const apolloProvider = new VueApollo({
